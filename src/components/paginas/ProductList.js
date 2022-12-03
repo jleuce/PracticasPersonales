cac0747bef80cc3220cad75b443e10eab0da0eb6
jleuce <110246740+jleuce@users.noreply.github.com>
2022-12-03 10:08:38 -0300

    Lista productos

--- src/components/paginas/ProductList.js
+++ src/components/paginas/ProductList.js
@@ -4,22 +4,58 @@ import { traerProductos, traerProducto, crearProducto, editarProducto, eliminarP
 import ProductRow from '../ProductRow';
 import UserContext from '../context/UserContext';
 
+const itemsPorLista = 3;
 
 function ProducList() {
 
     const [listaProductos,setListaProductos] = useState ([]);
+    const [listaParcialProductos,setListaParcialProductos] = useState ([]);
+    const [numeroLista,setNumeroLista] = useState (1);
     const [loadingProductos,setLoadingProductos] = useState (true);
-    const [token, setToken] = useState(null);
     const {user} =  useContext(UserContext);
+
+    const armarLista = (pagina) => {
+        if (pagina === 1){
+            const arrayTransitorio = [...listaProductos];
+            arrayTransitorio.splice(itemsPorLista*pagina,arrayTransitorio.length-itemsPorLista);
+            setListaParcialProductos(arrayTransitorio);
+            console.log(listaParcialProductos);
+        }else{
+            const arrayTransitorio = [...listaProductos];
+            arrayTransitorio.splice(itemsPorLista*pagina,arrayTransitorio.length-itemsPorLista);
+            arrayTransitorio.splice(0,itemsPorLista*(pagina-1));
+            setListaParcialProductos(arrayTransitorio);
+            console.log(listaParcialProductos);
+        }
+    }
+    const incrementarLista = () => {
+        if (listaProductos.length > numeroLista*itemsPorLista){
+            setNumeroLista(numeroLista + 1);
+        }
+    }
+    const bajarLista = () => {
+        if(numeroLista > 1){
+        setNumeroLista(numeroLista - 1);
+        }
+    }
+    const mostrarTodo = () => {
+        const arrayTransitorio = [...listaProductos];
+        setListaParcialProductos(arrayTransitorio);
+    }
 
     //Esto en realidad se ejecuta en la pagina de login
     useEffect( () => {
         setLoadingProductos(true);
         traerProductos(user.token)
             .then(r => setListaProductos(r))
+            .then(()=>armarLista(numeroLista))
             .then(()=>setLoadingProductos(false))
             .catch(r=>console.log('algo exploto',r))
     }, [])
+
+    useEffect( () => {
+        armarLista(numeroLista);
+    }, [numeroLista])
 
     if(loadingProductos === true){
         console.log("cargando");
@@ -49,9 +85,13 @@ function ProducList() {
                         </tr>
                     </thead>
                     <tbody>
-                        {listaProductos.map(p => <ProductRow producto={p} key={p.id} handleEliminarProducto={eliminarProducto}></ProductRow>)}
+                        {listaParcialProductos.map(p => <ProductRow producto={p} key={p.id} handleEliminarProducto={eliminarProducto}></ProductRow>)}
                     </tbody>
                 </table>
+                <button onClick={bajarLista}>Volver</button>
+                <button>Pagina:{numeroLista}</button>
+                <button onClick={incrementarLista}>Avanzar</button>
+                <button onClick={mostrarTodo}>Lista completa</button>
             </div>
             </>
         )
